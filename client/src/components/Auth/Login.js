d222d6aaf82c0e0d64f1fa6310a2d9d7234dde19
Ahmed Elshenawy <ahmedelshenawy250@gmail.com>
2020-03-01 16:33:58 +0200

    Change Login to use formik

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,78 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { Formik, Field, Form } from 'formik';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-class Login extends React.Component {
-    state = {
-      login: '',
-      password: '',
-      error: ''
-    }
 
-    loginHandler = async (event) => {
-      event.preventDefault();
-      try {
-        const response = await axios.post('/api/login', {
-          ...this.state
-        });
-        if (response.status !== 200) {
-          throw new Error('Login failed');
-        }
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('username', response.data.username);
-        this.props.onLogin();
-        this.props.history.push('/');
-      } catch (e) {
-        this.setState({
-          isAuth: false,
-          token: '',
-          username: '',
-          error: e.response ? e.response.data.message : e.message
-        });
-      }
-    }
+const Login = ({ onLogin }) => {
+  const history = useHistory();
+  const [error, setError] = useState('');
 
-    handleInputChange = (event) => {
-      const { name, value } = event.target;
-      this.setState({
-        [name]: value
-      });
-    }
-
-    render() {
-      const { login, password } = this.state;
-      return (
-        <div>
-          { this.state.error && (
-            <div className="ui warning message">
-              <div className="header">
-                {this.state.error}
-              </div>
-            </div>
-          )}
-          <form onSubmit={this.loginHandler} className="ui form">
-            <div className="field">
-              <input
-                type="text"
-                name="login"
-                value={login}
-                onChange={this.handleInputChange}
-                placeholder="Username"
-              />
-            </div>
-            <div className="field">
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-                placeholder="Password"
-              />
-            </div>
-            <button className="ui button" type="submit">Login</button>
-          </form>
+  return (
+    <>
+      {error && (
+        <div className="ui warning message">
+          <div className="header">
+            {error}
+          </div>
         </div>
-      );
-    }
-}
+      )}
+      <Formik
+        initialValues={{ login: '', password: '' }}
+        onSubmit={async (values) => {
+          try {
+            const response = await axios.post('/api/login', {
+              ...values
+            });
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('username', response.data.username);
+
+            onLogin();
+            history.push('/');
+          } catch (e) {
+            setError(e.response ? e.response.data.message : e.message);
+          }
+        }}
+      >
+        <Form className="ui form">
+          <div className="field">
+            <Field type="text" name="login" placeholder="Username" />
+          </div>
+          <div className="field">
+            <Field type="password" name="password" placeholder="Password" />
+          </div>
+          <button className="ui button" type="submit">Login</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired
+};
 
 export default Login;
