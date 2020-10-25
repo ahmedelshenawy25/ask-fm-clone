d81ea815bda09776886c69a266ac14d8ff107452
Ahmed Elshenawy <ahmedelshenawy250@gmail.com>
2020-10-26 01:12:01 +0200

    refactor router

--- client/src/router/PrivateRoute.js
+++ client/src/router/PrivateRoute.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import AuthContext from '../../context/AuthContext/AuthContext';
+import AuthContext from '../context/AuthContext/AuthContext';
 
 
-const AuthenticatedRoute = ({ children, path }) => {
+const PrivateRoute = ({ children, path }) => {
   const isAuth = useContext(AuthContext);
   return isAuth ? <Route path={path}>{children}</Route> : <Redirect to="/login" />;
 };
 
-AuthenticatedRoute.propTypes = {
+PrivateRoute.propTypes = {
   children: PropTypes.element.isRequired,
   path: PropTypes.string.isRequired
 };
 
-export default AuthenticatedRoute;
+export default PrivateRoute;
--- client/src/router/PublicRoute.js
+++ client/src/router/PublicRoute.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import AuthContext from '../../context/AuthContext/AuthContext';
+import AuthContext from '../context/AuthContext/AuthContext';
 
 
-const UnauthenticatedRoute = ({ children, path }) => {
+const PublicRoute = ({ children, path }) => {
   const isAuth = useContext(AuthContext);
   return !isAuth ? <Route path={path}>{children}</Route> : <Redirect to="/" />;
 };
 
-UnauthenticatedRoute.propTypes = {
+PublicRoute.propTypes = {
   children: PropTypes.element.isRequired,
   path: PropTypes.string.isRequired
 };
 
-export default UnauthenticatedRoute;
+export default PublicRoute;
