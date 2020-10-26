391391f90258992722754b8646da769d01806949
Ahmed Elshenawy <ahmedelshenawy250@gmail.com>
2020-10-26 18:35:24 +0200

    refactor navbar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,38 +1,14 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import AuthContext from '../../context/AuthContext/AuthContext';
-import SearchBar from '../Search/SearchBar';
+import PrivateNavbar from './PrivateNavbar';
+import PublicNavbar from './PublicNavbar';
 
 const Navbar = ({ onLogout, username }) => {
   const isAuth = useContext(AuthContext);
-  let authNavItems;
-  if (!isAuth) {
-    authNavItems = (
-      <div className="ui secondary pointing menu">
-        <div className="right menu">
-          <NavLink to="/signup" className="ui item">Signup</NavLink>
-          <NavLink to="/login" className="ui item">Login</NavLink>
-        </div>
-      </div>
-    );
-  } else {
-    authNavItems = (
-      <div className="ui secondary pointing menu">
-        <NavLink to="/home" exact className="item">Home</NavLink>
-        <NavLink to={`/user/${username}`} className="item">Profile</NavLink>
-        <NavLink to="/account/inbox" className="item">Inbox</NavLink>
-        <SearchBar />
-        <div className="right menu">
-          <NavLink to="/logout" className="ui item" onClick={() => onLogout()}>Logout</NavLink>
-        </div>
-      </div>
-    );
-  }
   return (
     <div style={{ marginBottom: '0.5%' }}>
-      { authNavItems }
+      { isAuth ? <PrivateNavbar username={username} onLogout={onLogout} /> : <PublicNavbar /> }
     </div>
   );
 };
