f15f06731bab92359280edda75c8062baf822605
Ahmed Elshenawy <ahmedelshenawy250@gmail.com>
2020-11-23 11:14:43 +0200

    redesign login and signup

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -4,61 +4,98 @@ import {
 } from 'formik';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import axiosInstance from '../../axiosInstance/axiosInstance';
 import loginValidationSchema from './login.validationSchema';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
 const Login = ({ onLogin }) => {
+  const classes = useStyles();
   const history = useHistory();
   const [error, setError] = useState('');
 
   return (
-    <>
-      {error && (
-        <div>
-          <div>
-            {error}
-          </div>
-        </div>
-      )}
-      <Formik
-        initialValues={{
-          usernameOrEmail: '',
-          password: ''
-        }}
-        validationSchema={loginValidationSchema}
-        onSubmit={async (values) => {
-          const castValues = loginValidationSchema.cast(values);
-          try {
-            const response = await axiosInstance.post('/login', {
-              ...castValues
-            });
-            localStorage.token = response.data.token;
-            localStorage.username = response.data.username;
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        { error }
+        <Formik
+          initialValues={{
+            usernameOrEmail: '',
+            password: ''
+          }}
+          validationSchema={loginValidationSchema}
+          onSubmit={async (values) => {
+            const castValues = loginValidationSchema.cast(values);
+            try {
+              const response = await axiosInstance.post('/login', {
+                ...castValues
+              });
+              localStorage.token = response.data.token;
+              localStorage.username = response.data.username;
 
-            onLogin();
-            history.push('/home');
-          } catch (e) {
-            setError(e.response ? e.response.data.message : e.message);
-          }
-        }}
-      >
-        {({ isValid, dirty, isSubmitting }) => (
-          <Form>
-            <div>
-              <Field name="usernameOrEmail" placeholder="Username or email" />
+              onLogin();
+              history.push('/home');
+            } catch (e) {
+              setError(e.response ? e.response.data.message : e.message);
+            }
+          }}
+        >
+          {({ isValid, dirty, isSubmitting }) => (
+            <Form>
+              <Field
+                name="usernameOrEmail"
+                placeholder="Username or email"
+                as={TextField}
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+              />
               <ErrorMessage name="usernameOrEmail" />
-            </div>
-
-            <div>
-              <Field type="password" name="password" placeholder="Password" />
+              <Field
+                type="password"
+                name="password"
+                placeholder="Password"
+                as={TextField}
+                variant="outlined"
+                size="small"
+                fullWidth
+                margin="normal"
+              />
               <ErrorMessage name="password" />
-            </div>
-
-            <button disabled={!(isValid && dirty) || isSubmitting} type="submit">Login</button>
-          </Form>
-        )}
-      </Formik>
-    </>
+              <Button
+                disabled={!(isValid && dirty) || isSubmitting}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Log In
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </Container>
   );
 };
 
--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -3,81 +3,147 @@ import {
   Formik, Form, Field, ErrorMessage
 } from 'formik';
 import { useHistory } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import axiosInstance from '../../axiosInstance/axiosInstance';
 import signupValidationSchema from './signup.validationSchema';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
 const Signup = () => {
+  const classes = useStyles();
   const history = useHistory();
   const [error, setError] = useState('');
 
   return (
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }}
-      validationSchema={signupValidationSchema}
-      onSubmit={async (values) => {
-        const { confirmPassword, ...castValues } = signupValidationSchema.cast(values);
-        try {
-          await axiosInstance.post('/signup', {
-            ...castValues
-          });
-          history.push('/login');
-        } catch (e) {
-          setError(e.response ? e.response.data.message : e.message);
-        }
-      }}
-    >
-      {({ isValid, dirty, isSubmitting }) => (
-        <>
-          {error && (
-          <div>
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+          }}
+          validationSchema={signupValidationSchema}
+          onSubmit={async (values) => {
+            const { confirmPassword, ...castValues } = signupValidationSchema.cast(values);
+            try {
+              await axiosInstance.post('/signup', {
+                ...castValues
+              });
+              history.push('/login');
+            } catch (e) {
+              setError(e.response ? e.response.data.message : e.message);
+            }
+          }}
+        >
+          {({ isValid, dirty, isSubmitting }) => (
             <div>
-              {error}
+              { error }
+              <Form>
+                <Field
+                  name="firstName"
+                  placeholder="First name"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage
+                  name="firstName"
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <Field
+                  name="lastName"
+                  placeholder="Last name"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage name="lastName" />
+                <Field
+                  name="username"
+                  placeholder="Username"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage name="username" />
+                <Field
+                  name="email"
+                  placeholder="Email"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage name="email" />
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage name="password" />
+                <Field
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm password"
+                  as={TextField}
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  margin="normal"
+                />
+                <ErrorMessage name="confirmPassword" />
+                <Button
+                  disabled={!(isValid && dirty) || isSubmitting}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Submit
+                </Button>
+              </Form>
             </div>
-          </div>
           )}
-          <Form>
-            <div>
-              <Field name="firstName" placeholder="First name" />
-              <ErrorMessage name="firstName" />
-            </div>
-
-            <div>
-              <Field name="lastName" placeholder="Last name" />
-              <ErrorMessage name="lastName" />
-            </div>
-
-            <div>
-              <Field name="username" placeholder="Username" />
-              <ErrorMessage name="username" />
-            </div>
-
-            <div>
-              <Field name="email" placeholder="Email" />
-              <ErrorMessage name="email" />
-            </div>
-
-            <div>
-              <Field type="password" name="password" placeholder="Password" />
-              <ErrorMessage name="password" />
-            </div>
-
-            <div>
-              <Field type="password" name="confirmPassword" placeholder="Confirm password" />
-              <ErrorMessage name="confirmPassword" />
-            </div>
-
-            <button disabled={!(isValid && dirty) || isSubmitting} type="submit">Submit</button>
-          </Form>
-        </>
-      )}
-    </Formik>
+        </Formik>
+      </div>
+    </Container>
   );
 };
 
